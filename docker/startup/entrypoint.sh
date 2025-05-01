c3c11528f07e6c73cb7c2bf112a1b4aa0adade5b
BradenMeyers <bjz-github@spwiz.com>
2025-04-30 21:10:59 -0600

    added the fleet params to the system

--- docker/startup/entrypoint.sh
+++ docker/startup/entrypoint.sh
@@ -12,23 +12,24 @@ source /opt/ros/humble/setup.bash
 cd /home/frostlab/ros2_ws
 MAKEFLAGS="-j 2" colcon build
 
+    
+SESSION_NAME="cougars"
+START_DIR="$HOME/ros2_ws"
+
+# Create a new tmux session (detached)
+tmux new-session -d -s $SESSION_NAME -c "$START_DIR" -n main
+
+# Set terminal options
+tmux set-option -t $SESSION_NAME default-terminal "screen-256color"
+tmux set-option -t $SESSION_NAME mouse on
+
+# Pane 0: start with sleep, clear, then run fastdds
+tmux send-keys -t $SESSION_NAME:0.0 "sleep 10" C-m
+tmux send-keys -t $SESSION_NAME:0.0 "clear" C-m
+tmux send-keys -t $SESSION_NAME:0.0 "fastdds discovery --server-id \$VEHICLE_ID" C-m
+
 # TODO change this if from aarch to a bash param
 if [ "$(uname -m)" == "aarch64" ]; then
-    
-    SESSION_NAME="cougars"
-    START_DIR="$HOME/ros2_ws"
-
-    # Create a new tmux session (detached)
-    tmux new-session -d -s $SESSION_NAME -c "$START_DIR" -n main
-
-    # Set terminal options
-    tmux set-option -t $SESSION_NAME default-terminal "screen-256color"
-    tmux set-option -t $SESSION_NAME mouse on
-
-    # Pane 0: start with sleep, clear, then run fastdds
-    tmux send-keys -t $SESSION_NAME:0.0 "sleep 10" C-m
-    tmux send-keys -t $SESSION_NAME:0.0 "clear" C-m
-    tmux send-keys -t $SESSION_NAME:0.0 "fastdds discovery --server-id \$VEHICLE_ID" C-m
 
     # Split horizontally (even-horizontal is default in split-window)
     tmux split-window -h -t $SESSION_NAME:0 -c "$START_DIR"
