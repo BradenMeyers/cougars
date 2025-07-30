ec78c92d64386638d8a7266298e053e181b7bbdf
Braden <bjz-github@spwiz.com>
2025-07-29 22:49:17 -0600

    sourcing with home directory instead of absolute path

--- config/cougarsrc.sh
+++ config/cougarsrc.sh
@@ -4,9 +4,9 @@
 export LABNAME='frostlab'
 
 source /opt/ros/humble/setup.bash
-source /home/$LABNAME/ros2_ws/install/setup.bash
+source ~/ros2_ws/install/setup.bash
 export FASTRTPS_DEFAULT_PROFILES_FILE=~/config/fast_discovery_config.xml
-export FLEET_PARAMS_FILE=/home/frostlab/config/deploy_tmp/fleet_params.yaml # ex. /home/frostlab/config/fleet_params.yaml
+export FLEET_PARAMS_FILE=~/config/deploy_tmp/fleet_params.yaml # ex. ~/config/fleet_params.yaml
 
 # TODO: This will only work inside the container but it hopefully only needs to work inside the container
 source ~/config/cougrc.sh
