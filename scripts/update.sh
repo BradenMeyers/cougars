bc3e380e103970813323be60c4bbb294e2ae9dd0
Braden <bjz-github@spwiz.com>
2025-08-05 14:58:43 -0600

    Update script with force flag

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 # - Make sure you run this from the root of the top-level repo
 
-source config/cougarsrc.sh
+
+# Check for force flag
+FORCE=0
+if [[ "$1" == "-f" ]]; then
+    FORCE=1
+fi
+
+# Set up workspace
+# To be run from the root of the top-level repo
+if [ $FORCE -eq 1 ]; then
+    echo "Force flag set: cleaning ros2_ws and other directories"
+    rm -rf ros2_ws/src
+    rm -rf cougars-teensy cougars-gpio cougars-base-station cougars-docs
+fi
+
+
 
 # Docker updates
 docker pull frostlab/cougars:vehicle
 docker pull frostlab/cougars:base_station
+
+mkdir -p ros2_ws/src
 
 vcs pull ros2_ws/src
 
